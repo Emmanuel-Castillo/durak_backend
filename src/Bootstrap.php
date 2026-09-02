65399dc31979dc1e8c200fd9649dae386e5668fa
Emmanuel-Castillo <emmanuel.2001alonso.ec@gmail.com>
2026-09-01 20:41:49 -0500

    Require filp/whoops package

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,4 +1,24 @@
 <?php
 declare(strict_types=1);
 
-echo "Hello World";
+namespace DurakBackend;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+// Registering the error handler
+$whoops = new \Whoops\Run;
+if ($environment === 'development') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function ($e) {
+        echo 'Todo: Friendly error page and send email to dev.';
+    });
+}
+
+$whoops->register();
+
+throw new \Exception;
